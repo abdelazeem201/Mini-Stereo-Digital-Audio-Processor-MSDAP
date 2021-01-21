d3445e0f7844278d0e9d173ffd494901b34559b9
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2021-01-21 19:23:58 +0200

    Create adder.v

---
+++ rtl/adder.v
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Zagazig University
+// Engineer: Ahmed Abdelazeem
+// 
+// Create Date:    20:58:48 01/16/2021 
+// Design Name: MSDAP
+// Module Name: adder    
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+////////////////////////////////////////////////////////////////////////////////
+module adder(
+	      input wire [39:0] a,
+        input wire [39:0] b,
+        input wire addsub,
+	      input wire adder_en,
+        output wire [39:0] sum );
+
+
+assign sum = (addsub == 1'b1) ? (b - a) :  (addsub == 1'b0) ? (b + a) : sum ;	
+
+endmodule
